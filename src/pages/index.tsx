f34b6178ba9ecf05468226eac9b35a75feab6ded
civic86 <kaikoharazawa@gmail.com>
2024-01-12 19:09:03 -0800

    edit header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
-
+import Image from "next/image";
 const DynamicMap = dynamic(() => import("../../components/Map"), { ssr: false });
 
 export default function Home() {
@@ -17,6 +17,12 @@ export default function Home() {
         }}
       >
 
+        <Image
+          src="/images/leaf.png"
+          alt="Canada Line"
+          width={100}
+          height={100}
+        />
         <h1 style={{ color: "white", fontSize: 28 }}>
           Canada Line
         </h1>
